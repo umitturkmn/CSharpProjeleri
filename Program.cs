dc9f075bf7ab129516b9fd18518a8a16f072ef8a
Ümit Türkmen <umit.482@gmail.com>
2022-02-06 10:35:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpProjeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhabalar.");
+            Console.Write("İsminiz: ");
+            string name = Console.ReadLine();
+            Console.Write("Soyisminiz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Adınız ve Soyadınız: " + name +  " " + surname);
+        }
+    }
+}
